f86555ac9f0e0083405c7200e0380d4d0dd73197
Loic Fourmaux <loicfourmaux@gmail.com>
2023-01-26 17:37:54 +0100

    add tests for strlen

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int ft_strlen(char *str);
+char *bigStr = "zHObYpJufpveb2xIatH0wGHGwmcRSKsWgIpOXReM0IvQmuCd42ZHicOTul9hpLgJxO6d1cHE5I5aF0izSXEu6JygosCAxVDfBH1SA9vAeG4BsJuq6ow6mBcYTy3T6CwAzlMGZ2sdPvd8NtDNqlOWJvRUDfyujuV94CKt54XN2a1DIT8CwIlvABGVFMDGicwh3qP7TDVQkaXzuMsljE0ZwOsdaQAJ1ggpkbtxQXv1G2FybThSQDlr16rUM4x8aur5mXs12ki81168PP9xlgZHZ9oiX46WUvlq9bcWU4nhh76ktUUfmLRw7ZrYz43ctTYMX6deb4Q2f3hGwCWQbfBovs1nEIeQ4t6EWJOPQS2hPkmUCUzxusPUgKi7kkU3eGD2IMpeS9ebPepyeS9BQsb5QJ8xcjxu3SotzWBoU7yIKzkAbrROBx4RbEwsuwqIshnSjU8nSwzrMwYCf2F1E039Jxdc1JR84RVFTFu53OXssFh3mjXosicg";
 
 int main(){
-	int len = ft_strlen("salut les potes");
-
-	printf("%d", len);
+	printf("10 chars string -> %d\n", ft_strlen("0123456789"));
+	printf("Empty string -> %d\n", ft_strlen(""));
+	printf("50 chars string -> %d\n", ft_strlen("qwertyuiop;loikjuyhgtrfdewsaqzxswqaswedfrtghyujkiu"));
+	printf("1 char string -> %d\n", ft_strlen("a"));
+	printf("500 char string -> %d\n", ft_strlen(bigStr));
 	return 0;
 }
--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -1,15 +1,13 @@
 section .text
 	global _ft_strlen
 	_ft_strlen:
-		mov rax, 0              ; initialize strlen counter
-		cmp byte [rdi], 0x00    ; if value at [rdi] is 0x00 return
-		jne .loop               ; loop if not at end of string
+		mov rax, 0
+		cmp byte [rdi], 0x00
+		jne .loop
 		ret
     .loop:
-		add rdi, 1              ; increment char * to next character
-		add rax, 1              ; increment strlen counter
-		cmp byte [rdi], 0x00    ; if value at [rdi] is 0x00 return
-		jne .loop               ; loop if not at end of string
+		inc rdi,
+		inc rax,
+		cmp byte [rdi], 0x00
+		jne .loop
 		ret
-
-	                       ; jump back to input
